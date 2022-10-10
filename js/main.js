49e2c489988d402c583e7239ada151e1b55dc73b
Alexandra Kuznetsova <a.kuznetsova1992@bk.ru>
2022-10-10 21:40:12 +0300

    Исправила!

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
 function randomIntFromInterval(min, max) {
-    if (min < 0 || max < 0) {
-      return NaN;
-    }
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
   min = Math.floor(min);
   max = Math.ceil(max);
-   if (min === max) {
+  if (min === max) {
     return min;
-   }
-} 
-
-function randomIntFromInterval2(min, max) {
-    return (Math.round(Math.random() * (max - min)) + min);
+  }
+  return min > max ? random(max, min) : random(min, max);
 }
 
-randomIntFromInterval2(3,7);
-console.log(randomIntFromInterval2(3,7))
+function random(min, max) {
+  return (Math.round(Math.random() * (max - min)) + min);
+}
 
-function stringLength(inputtxt, maxlength) 
-{
-      if(inputtxt.length > maxlength)
-      { 
-      return false;
-      }
-      else
-      return !(inputtxt.length > maxlength)
+randomIntFromInterval(3, 7);
+console.log(randomIntFromInterval(3, 7));
+
+function stringLength(inputtxt, maxlength) {
+  return !(inputtxt.length > maxlength)
 }
 
 let inputtxt = 'Ура-ура!';

9045460189b0e8e2b95991d023880d7cc24d1eb9
Alexandra Kuznetsova <a.kuznetsova1992@bk.ru>
2022-10-11 14:34:59 +0300

    Задание 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const MIN_NUMBER = 2;
+const MAX_NUMBER = 7;
+const INPUT_TXT = 'Ура-ура!';
+const MAX_LENGHT = 20;
+
 function randomIntFromInterval(min, max) {
   if (min < 0 || max < 0) {
     return NaN;
@@ -14,15 +19,12 @@ function random(min, max) {
   return (Math.round(Math.random() * (max - min)) + min);
 }
 
-randomIntFromInterval(3, 7);
-console.log(randomIntFromInterval(3, 7));
+randomIntFromInterval(MIN_NUMBER, MAX_NUMBER);
+console.log(randomIntFromInterval(MIN_NUMBER, MAX_NUMBER));
 
-function stringLength(inputtxt, maxlength) {
-  return !(inputtxt.length > maxlength)
+function stringLength(input, line) {
+  return !(input.length > line)
 }
 
-let inputtxt = 'Ура-ура!';
-let maxlength = 20;
-
-stringLength(inputtxt, maxlength);
-console.log(stringLength(inputtxt, maxlength));
+stringLength(INPUT_TXT, MAX_LENGHT);
+console.log(stringLength(INPUT_TXT, MAX_LENGHT));

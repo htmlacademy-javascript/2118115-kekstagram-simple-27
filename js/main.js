4439a3e3d304b419ffab4431a428eb4daf89e4b0
Alexandra Kuznetsova <a.kuznetsova1992@bk.ru>
2022-10-18 17:39:57 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const MIN_NUMBER = 2;
-const MAX_NUMBER = 7;
+const MIN_NUMBER = 1;
+const MAX_NUMBER =25;
 const INPUT_TXT = 'Ура-ура!';
 const MAX_LENGHT = 20;
 
@@ -28,3 +28,50 @@ function stringLength(input, line) {
 
 stringLength(INPUT_TXT, MAX_LENGHT);
 console.log(stringLength(INPUT_TXT, MAX_LENGHT));
+
+const TRAVEL = [
+  'beach',
+  'goToTheBeach',
+  'sea',
+  'girlwithTits',
+  'soup',
+  'blackCar',
+  'strawberry',
+  'juice',
+  'airplane',
+  'shoes',
+  'road',
+  'whiteCar',
+  'salad',
+  'sushiCat',
+  'uggBoots',
+  'sky',
+  'choir',
+  'redCar',
+  'slippers',
+  'palmTrees',
+  'dinner',
+  'sunset',
+  'crab',
+  'concert',
+  'bigCar',
+];
+
+const createNewObject = () => {
+  const RANDOM_ID_INDEX = randomIntFromInterval(1,25);
+  const RANDOM_URL_INDEX = randomIntFromInterval(1,25);
+  const RANDOM_DESCRIPTION_INDEX = randomIntFromInterval(0, TRAVEL.length - 1);
+  const RANDOM_LIKES_INDEX = randomIntFromInterval(15,200);
+  const RANDOM_COMMENTS_INDEX = randomIntFromInterval(0,200);
+  return {
+    id: RANDOM_ID_INDEX,
+    url: 'photos/' + RANDOM_URL_INDEX + '.jpg',
+    description: TRAVEL[RANDOM_DESCRIPTION_INDEX],
+    likes: RANDOM_LIKES_INDEX,
+    comments: RANDOM_COMMENTS_INDEX,
+  }
+}
+
+const SIMILAR_OBJECT = Array.from({length: 25}, createNewObject);
+
+console.log(SIMILAR_OBJECT);

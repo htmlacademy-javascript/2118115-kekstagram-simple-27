f98012aacb422e4246e1bd15c69689ca9d8ea8dc
Alexandra Kuznetsova <a.kuznetsova1992@bk.ru>
2022-11-08 23:25:15 +0300

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import './util.js';
 import {createSimilarPhotoObjects} from './data.js';
-
+import {picturesForTemplateBlock} from './userphoto.js';
 console.log (createSimilarPhotoObjects());
+console.log (picturesForTemplateBlock());

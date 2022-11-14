05e282f53e80415f930e5c2904c3fd020d6c7cd2
Alexandra Kuznetsova <a.kuznetsova1992@bk.ru>
2022-11-14 23:04:19 +0300

    Сообщение об ошибке заполнения формы

--- js/user-form.js
+++ js/user-form.js
@@ -1,18 +1,26 @@
 const userForm = document.querySelector('.img-upload__form');
 
-const pristine = new pristineMin(userForm, {
-  classTo: 'img-upload__text',
-  errorTextParrent: 'img-upload_text',
-  errorTextClass: 'img-upload_error-text',
+const pristine = new Pristine (userForm, {
+  classTo: 'img-upload__wrapper',
+  errorClass: 'img-upload__wrapper--invalid',
+  successClass: 'img-upload__wrapper--valid',
+  errorTextParent: 'img-upload__wrapper',
+  errorTextTag: 'span',
+  errorTextClass: 'img-upload__form--error'
 });
+
+function validateComment (value) {
+  return value.length >= 20 && value.length <= 140;
+}
+
+pristine.addValidator(
+  userForm.querySelector('.text__description'), 
+  validateComment,
+  'От 20 до 140 символов'
+);
 
 userForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
-  
-    const isValid = pristine.validate();
-    if (isValid) {
-      console.log('Можно отправлять');
-    } else {
-      console.log('Форма невалидна');
-    }
-  });
+    pristine.validate();
+});
+
